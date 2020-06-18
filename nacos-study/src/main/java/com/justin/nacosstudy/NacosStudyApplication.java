4ab2b44e617703683c81e568c7c22d558430f66c
xiaojun <1255332375@qq.com>
2020-06-18 18:20:15 +0800

    push to remote

---
+++ nacos-study/src/main/java/com/justin/nacosstudy/NacosStudyApplication.java
@@ -0,0 +1,13 @@
+package com.justin.nacosstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosStudyApplication.class, args);
+    }
+
+}
---
+++ nacos-study/src/test/java/com/justin/nacosstudy/NacosStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justin.nacosstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
