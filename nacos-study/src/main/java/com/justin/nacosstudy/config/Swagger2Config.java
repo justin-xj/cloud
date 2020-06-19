a95ab9ac309515374974dfc68c0eb25b1a3d8b70
xiaojun <1255332375@qq.com>
2020-06-19 10:54:22 +0800

    新增swagger

---
+++ nacos-study/src/main/java/com/justin/nacosstudy/config/Swagger2Config.java
@@ -0,0 +1,39 @@
+package com.justin.nacosstudy.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    /**
+     * 添加摘要信息(Docket)
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Client Gateway-Swagger2 APIs")
+                .termsOfServiceUrl("http://localhost:8080")
+                //.contact(new Contact("unnet","https://blog.csdn.net/lovequanquqn?viewmode=list","love1208tt@foxmail.com"))
+                .version("1.0.0")
+                .build();
+    }
+}
--- nacos-study/src/main/java/com/justin/nacosstudy/controller/HelloController.java
+++ nacos-study/src/main/java/com/justin/nacosstudy/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.justin.nacosstudy.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @ApiOperation("测试接口")
     @RequestMapping("/hello")
     public String hello(){
         return "hello world!";
