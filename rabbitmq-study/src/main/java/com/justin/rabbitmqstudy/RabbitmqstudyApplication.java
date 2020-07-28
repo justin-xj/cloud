b15ce5875da69db01a017460b961c8711d6bc731
xiaojun <1255332375@qq.com>
2020-07-28 15:37:50 +0800

    新增提交master

---
+++ rabbitmq-study/src/main/java/com/justin/rabbitmqstudy/RabbitmqstudyApplication.java
@@ -0,0 +1,13 @@
+package com.justin.rabbitmqstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqstudyApplication.class, args);
+    }
+
+}
