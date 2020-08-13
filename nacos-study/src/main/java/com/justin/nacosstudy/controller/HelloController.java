eda89a01c1e366e82ee190a303efb9d5004ee399
xiaojun <1255332375@qq.com>
2020-08-13 14:29:44 +0800

    新增提交master

--- nacos-study/src/main/java/com/justin/nacosstudy/controller/HelloController.java
+++ nacos-study/src/main/java/com/justin/nacosstudy/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.justin.nacosstudy.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.context.ShutdownEndpoint;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +16,20 @@ public class HelloController {
     @Value(value = "${test.justin}")
     private String justin;
 
+    @Autowired
+    private ApplicationContext context;
+
     @ApiOperation("测试接口")
     @RequestMapping("/hello")
     public String hello(){
         return justin;
+    }
+
+    @RequestMapping("/shutdown")
+    public String shutdown(){
+        ShutdownEndpoint shutdownEndpoint = new ShutdownEndpoint();
+        shutdownEndpoint.setApplicationContext(context);
+        shutdownEndpoint.shutdown();
+        return "success";
     }
 }
